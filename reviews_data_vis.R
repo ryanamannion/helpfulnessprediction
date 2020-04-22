a369b904d17310b034f51c7941b51f7ceb004073
Ryan A. Mannion <ryanamannion@users.noreply.github.com>
2020-04-21 23:42:03 -0400

    messing around with some r for data vis

---
+++ reviews_data_vis.R
@@ -0,0 +1,18 @@
+# Helpfulness Prediction Data Visualization
+# Ryan A. Mannion
+
+setwd("/users/ryanmannion/LING472/helpfulnessprediction")
+review_data <- read.table("review_data.tab", header=T)
+names(review_data) <- c("hnum", "hdenom", "score")
+head(review_data)
+
+attach(review_data)
+help_score <- hnum/hdenom
+help_score[is.nan(help_score)] <- 0
+
+hist(help_score, main="Histogram of Helpfulness Scores", 
+     xlab="Helpfulness Score (helpful/total votes)")
+
+detach(review_data)
+
+
