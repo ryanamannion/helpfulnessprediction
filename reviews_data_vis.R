6290a1287328eb248a242209fb40ee6e7de08caf
Ryan A. Mannion <ryanamannion@users.noreply.github.com>
2020-04-22 13:29:24 -0400

    changes hist command to exclude NaNs

--- reviews_data_vis.R
+++ reviews_data_vis.R
@@ -7,11 +7,21 @@ names(review_data) <- c("hnum", "hdenom", "score")
 head(review_data)
 
 attach(review_data)
+# Calculate Helpfulness Scores
 help_score <- hnum/hdenom
-help_score[is.nan(help_score)] <- 0
 
-hist(help_score, xlim=c(0,1), main="Histogram of Helpfulness Scores", 
-     xlab="Helpfulness Score (helpful/total votes)")
+# Find NaN
+num_nan <- sum(is.nan(help_score))
+
+# Removes the 270052 NaN reviews 
+help_score_no_nan <- subset(help_score, !is.nan(help_score))
+
+# Checks Length
+length(help_score) - num_nan == length(help_score_no_nan)
+
+# Histogram of Scores
+hist(help_score_no_nan, xlim=c(0,1), main="Histogram of Helpfulness Scores (Without NaN Vlues)", 
+     xlab="Helpfulness Score (helpful votes/total votes)")
 
 detach(review_data)
 
