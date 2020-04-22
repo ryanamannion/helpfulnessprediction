af52da681561ac436688b72eb5d5ec5c82af652d
Ryan A. Mannion <ryanamannion@users.noreply.github.com>
2020-04-22 13:40:51 -0400

    removes two scores above 1

--- reviews_data_vis.R
+++ reviews_data_vis.R
@@ -8,19 +8,25 @@ head(review_data)
 
 attach(review_data)
 # Calculate Helpfulness Scores
-help_score <- hnum/hdenom
+help_score_raw <- hnum/hdenom
 
 # Find NaN
-num_nan <- sum(is.nan(help_score))
+num_nan <- sum(is.nan(help_score_raw))
 
 # Removes the 270052 NaN reviews 
-help_score_no_nan <- subset(help_score, !is.nan(help_score))
+help_score_no_nan <- subset(help_score_raw, !is.nan(help_score_raw))
 
 # Checks Length
-length(help_score) - num_nan == length(help_score_no_nan)
+length(help_score_raw) - num_nan == length(help_score_no_nan)
+
+# Finds Scores out of Range
+max(help_score_no_nan) # there should not be a value of 3
+sum(help_score_no_nan > 1) # there are two values greater than 1
+help_scores <- help_score_no_nan[!help_score_no_nan > 1] # remove those values
 
 # Histogram of Scores
-hist(help_score_no_nan, xlim=c(0,1), main="Histogram of Helpfulness Scores (Without NaN Vlues)", 
+hist(help_score_no_nan, xlim=c(0,1), 
+     main="Histogram of Helpfulness Scores (Without NaN Vlues)", 
      xlab="Helpfulness Score (helpful votes/total votes)")
 
 detach(review_data)
