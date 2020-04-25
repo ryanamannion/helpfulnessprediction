78bb03b6c3202b142886b58626de5ff8f8659516
Ryan A. Mannion <ryanamannion@users.noreply.github.com>
2020-04-25 13:58:28 -0400

    begins first simple feature function

--- get_features.py
+++ get_features.py
@@ -5,11 +5,31 @@ creates a class to get and fill a feature matrix for a logistic regression model
 Ryan A. Mannion
 Written for LING472 Final Project
 """
+import spacy
+import numpy as np
 
 
 class HelpfulnessVectorizer:
     """Creates a matrix of features for helpfulness prediction"""
 
     def __init__(self):
+        """
+        Instantiates SimilarityVectorizer, loads spaCy model
+        """
+        nlp = spacy.load("en_core_web_lg")
+
+    def get_simple_features(self, data):
+        """
+        Features in this function:
+            - Number of sentences
+            - Number of words
+            - Average words/ sentence
+            - Number of characters
+        :param data:
+        :return: numpy nd array of shape TODO: specify shape
+        """
         pass
 
+    # TODO: add feature function for more complex features
+
+    # TODO: get features, combine arrays with numpy.concatenate
