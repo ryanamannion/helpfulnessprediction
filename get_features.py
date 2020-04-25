e87d90d78aa6d8f7ef4aa437fce8b2c2597f7c12
Ryan A. Mannion <ryanamannion@users.noreply.github.com>
2020-04-25 13:02:19 -0400

    creates HelpfulnessVectorizer class

--- get_features.py
+++ get_features.py
@@ -5,3 +5,11 @@ creates a class to get and fill a feature matrix for a logistic regression model
 Ryan A. Mannion
 Written for LING472 Final Project
 """
+
+
+class HelpfulnessVectorizer:
+    """Creates a matrix of features for helpfulness prediction"""
+
+    def __init__(self):
+        pass
+
