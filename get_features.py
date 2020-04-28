69aa0c6c6599ffd25b1a197d99341a11664ba7a5
Ryan A. Mannion <ryanamannion@users.noreply.github.com>
2020-04-28 14:19:06 -0400

    adds text preprocessing, saves feature_array as attribute

--- get_features.py
+++ get_features.py
@@ -9,54 +9,79 @@ import spacy
 from spacy_readability import Readability
 import numpy as np
 import argparse
-from data_util import ReviewerData
+from data_util import ReviewerData, preprocess_text
 from tqdm import tqdm
 
 
 class HelpfulnessVectorizer:
-    """Creates a matrix of features for helpfulness prediction"""
+    """
+    Creates a matrix of features for helpfulness prediction
 
-    def __init__(self, data):
+    Attributes:
+        nlp (spacy object): spacy
+        read (class): Readability pipeline for spacy
+        data (dict): dictionary containing reviewer data
+        feature_array (numpy.ndarray): contains extracted features for logistic regression model
+
+    Methods:
+        get_features: Extracts features from review texts and fills in a numpy ndarray
+    """
+
+    def __init__(self, data, spacy_model='en_core_web_lg'):
         """
         Instantiates SimilarityVectorizer, loads spaCy model
         """
         print("Initializing spaCy...")
-        self.nlp = spacy.load("en_core_web_lg")
+        self.nlp = spacy.load(spacy_model)
         self.read = Readability()
         self.nlp.add_pipe(self.read, last=True)
         self.data = data
 
+        # Populated if get_features() performed
+        self.feature_array = None
+
     def get_features(self):
         """
-        Simple Features in this function:
-            - Star rating of five
-            - Number of sentences
-            - Number of tokens
-            - Number of characters
-            - Average tokens/ sentence
-            - Average characters/ token
+        Extracts features from review texts and fills in a numpy ndarray
 
-        Readability Features in this function:
-            - Flesch-Kincaid Grade Level
-            - Flesch-Kincaid Reading Ease
-            - Dale Chall
-            - SMOG
-            - Coleman-Liau Index
-            - Automated Readability Index
-            - FORCAST
+        Meta Features:
+            1 - Star rating of five
+
+        Simple Text Features:
+            2 - Number of sentences
+            3 - Number of tokens
+            4 - Number of characters
+            5 - Average tokens/ sentence
+            6 - Average characters/ token
+
+        Readability Features:
+            7 - Flesch-Kincaid Grade Level
+            8 - Flesch-Kincaid Reading Ease
+            9 - Dale Chall
+            10 - SMOG
+            11 - Coleman-Liau Index
+            12 - Automated Readability Index
+            13 - FORCAST
+
+        Other Features:
+            ## - TODO: TF/IDF
+            ## - TODO: Humor?
+            ## - TODO: Summary Features
 
         data has columns: Id,ProductId,UserId,ProfileName,HelpfulnessNumerator,HelpfulnessDenominator,
                             Score,Time,Summary,Text
+
         :return: numpy nd array of shape [# reviews, 13]
         """
         # selects sample from data dictionary for length
         sample = list(self.data.values())[0]
 
-        my_array = np.zeros((len(sample), 13))   # as many rows as sample is long, 6 features wide
+        my_array = np.zeros((len(sample), 13))   # as many rows as sample is long, 13 features wide
 
         texts = self.data["Text"]
-        print("Getting Simple Features...")
+        print(f"Extracting Features for {len(texts)} Texts...")
         for i, text in tqdm(enumerate(texts)):
+            text = preprocess_text(text=text)
             doc = self.nlp(text)
 
             num_sents = 0
@@ -90,26 +115,23 @@ class HelpfulnessVectorizer:
             my_array[i, 11] = doc._.automated_readability_index
             my_array[i, 12] = doc._.forcast
 
+        self.feature_array = my_array
+
         return my_array
 
 
-def main():
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('--data_path', type=str, default="reviews.csv",
-    #                     help="csv file containing data to be read")
-    # args = parser.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', type=str, default="./data/sample.tsv",
+                        help="tsv/csv file containing data to be read")
+    args = parser.parse_args()
 
-    data = './data/train.tsv'
+    data = args.data_path
 
-    print(f"Loading Data: {data} ...")
     reviewer_data = ReviewerData(data_file=data, delimiter='\t')
 
     my_vectorizer = HelpfulnessVectorizer(reviewer_data.data_dict)
 
     feature_array = my_vectorizer.get_features()
 
-    print(feature_array)
-
-
-if __name__ == "__main__":
-    main()
+    np.save("train_x13_feats", feature_array)
